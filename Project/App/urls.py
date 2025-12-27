b6cfa04827fc2ab99e25aea8b327d255f41c2c4e
appallakarthik <appallakarthik07@gmail.com>
2025-12-27 16:06:38 +0530

    commits

--- Project/App/urls.py
+++ Project/App/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
